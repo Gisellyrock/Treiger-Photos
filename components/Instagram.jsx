62514de117dbd0cfbe29dc7ddccb32ef8be06c94
GisellyRock <gisellydsr@gmail.com>
2023-12-04 11:24:59 -0300

    first commit

---
+++ components/Instagram.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import IgImg1 from '../public/ig-img-1.jpeg';
+import IgImg2 from '../public/ig-img-2.jpeg';
+import IgImg3 from '../public/ig-img-3.jpeg';
+import IgImg4 from '../public/ig-img-4.jpeg';
+import IgImg5 from '../public/ig-img-5.jpeg';
+import IgImg6 from '../public/ig-img-6.jpeg';
+import InstagramImg from './InstagramImg';
+
+const Instagram = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto text-center py-24">
+      <p className="text-2xl font-bold">Siga-me no Instagram</p>
+      <a
+        href="https://www.instagram.com/treiger_photos/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:underline"
+      >
+        @treiger_photos
+      </a>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4">
+        <InstagramImg
+          socialImg={IgImg1}
+          instagramLink="https://www.instagram.com/p/C0KepR3vd4tij6XWcTvSS6ekDWk4Muf-xYypSM0/"
+        />
+
+        <InstagramImg socialImg={IgImg2} />
+        <InstagramImg socialImg={IgImg3} />
+        <InstagramImg socialImg={IgImg4} />
+        <InstagramImg socialImg={IgImg5} />
+        <InstagramImg socialImg={IgImg6} />
+      </div>
+    </div>
+  );
+};
+
+export default Instagram;
